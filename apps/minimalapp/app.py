14f96132bd518fade3a83a846f8330de8d068d72
x24ken <x24ken>
2022-04-26 15:11:53 +0900

    お問い合わせ画面にFlashを追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, current_app, g, redirect, render_template, request, url_for
+from email_validator import EmailNotValidError, validate_email
+from flask import render_template  # current_app,; g,
+from flask import Flask, flash, redirect, request, url_for
 
 # Flaskクラスをインスタンス化する
 app = Flask(__name__)
+# SECRET_KEYを追加する
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 
 
 # URLと実行関数をマッピングする
@@ -37,8 +41,31 @@ def contact_complete():
             user_name = request.form["username"]
             email = request.form["email"]
             description = request.form["description"]
+            print(description)
+            # 入力チェック
+            is_valid = True
+
+            if not user_name:
+                flash("ユーザー名は必須です")
+                is_valid = False
+
+            if not email:
+                flash("メールアドレスは必須です")
+                is_valid = False
+
+            try:
+                validate_email(email)
+            except EmailNotValidError:
+                flash("メールアドレスの形式で入力してください")
+                is_valid = False
+
+            if not is_valid:
+                return redirect(url_for("contact"))
+
             # メールを送る
 
+            # 問い合わせ完了エンドポイントへリダイレクトする
+            flash("問い合わせありがとうございました。")
             return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
