12df46fb4acc19ee765c0777abe094dd5bc9d2d2
x24ken <x24ken>
2022-04-26 16:34:52 +0900

    お問い合わせメールの実装

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,11 +1,22 @@
+import logging
+import os
+
 from email_validator import EmailNotValidError, validate_email
 from flask import render_template  # current_app,; g,
 from flask import Flask, flash, redirect, request, url_for
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 # Flaskクラスをインスタンス化する
 app = Flask(__name__)
 # SECRET_KEYを追加する
 app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+# ログレベルを設定する
+app.logger.setLevel(logging.DEBUG)
+# リダイレクトを中断しないようにする
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# DebugToolbarExtensionにアプリケーションをセットする
+toolbar = DebugToolbarExtension(app)
 
 
 # URLと実行関数をマッピングする
@@ -38,14 +49,14 @@ def contact_complete():
     if request.method == "POST":
         if request.method == "POST":
             # form属性を使ってフォームの値を取得する
-            user_name = request.form["username"]
+            username = request.form["username"]
             email = request.form["email"]
             description = request.form["description"]
             print(description)
             # 入力チェック
             is_valid = True
 
-            if not user_name:
+            if not username:
                 flash("ユーザー名は必須です")
                 is_valid = False
 
@@ -63,9 +74,34 @@ def contact_complete():
                 return redirect(url_for("contact"))
 
             # メールを送る
+            send_email(
+                email,
+                "問い合わせありがとうございました。",
+                "contact_mail",
+                username=username,
+                description=description,
+            )
 
             # 問い合わせ完了エンドポイントへリダイレクトする
             flash("問い合わせありがとうございました。")
             return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
+
+
+def send_email(to, subject, template, **kwargs):
+    """メールを送信する関数"""
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
+
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
