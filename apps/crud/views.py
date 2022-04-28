492a40047999981a1d1e3e4c864b4223857bb726
x24ken <x24ken@gmail.com>
2022-04-28 16:46:42 +0900

    ユーザー編集ページ追加

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -37,3 +37,31 @@ def index():
 def sql():
     db.session.query(User).all()
     return "コンソールログを確認してください"
+
+
+@crud.route("/users")
+def users():
+    """ユーザの一覧を取得する"""
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
+
+
+# methodsにGETとPOSTを指定する
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+
+    # Userモデルを利用してユーザーを取得する
+    user = User.query.filter_by(id=user_id).first()
+
+    # formからサブミットさっれた場合はユーザーを更新しユーザーの一覧画面へリダイレクトする
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+
+    # GETの場合はHTMLを返す
+    return render_template("crud/edit.html", user=user, form=form)
